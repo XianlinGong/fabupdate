b62ff686083a4bfe1f8b934649b956dfe82fa82c
Ubuntu <ubuntu@ip-172-31-12-81.ap-northeast-1.compute.internal>
2018-10-24 15:10:40 +0000

    start-test.sh

--- fabcoin.service.sh
+++ fabcoin.service.sh
@@ -1,5 +1,7 @@
+set -x
 sudo mkdir /usr/lib/systemd/system
 sudo cp fabcoin.service /usr/lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start fabcoin
 sudo systemctl enable fabcoin
+sudo systemctl status fabcoin
