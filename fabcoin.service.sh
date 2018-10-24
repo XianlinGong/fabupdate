36c970b516c40ebffc138c7a5182da18114b7516
Ubuntu <ubuntu@ip-172-31-3-181.eu-west-2.compute.internal>
2018-10-24 17:33:23 +0000

    mainnet

--- fabcoin.service.sh
+++ fabcoin.service.sh
@@ -1,7 +1,14 @@
 set -x
+sudo systemctl stop fabcoin
+killall fabcoind
+sleep 5
+
 sudo mkdir /usr/lib/systemd/system
 sudo cp fabcoin.service /usr/lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl start fabcoin
 sudo systemctl enable fabcoin
+
+
 sudo systemctl status fabcoin
+tail  ~/.fabcoinsc/debug.log
