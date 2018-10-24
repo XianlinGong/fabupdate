d9a46a46ad90faa6d0f8fa96d2e5896dc2dd7ee9
Ubuntu <ubuntu@ip-172-31-12-81.ap-northeast-1.compute.internal>
2018-10-24 15:41:21 +0000

    start-test.sh

--- start-test-reindex.sh
+++ start-test-reindex.sh
@@ -3,3 +3,5 @@ sleep 5
 
 fabcoind -server -listen -rpcuser=fabcoin -rpcpassword=fabcoin -daemon -testnet -txindex=1 -rest -rpcallowip=0.0.0.0/0  -blockmaxconflict=12 -logevents -reindex
 
+tail -f ~/.fabcoinsc/testnet3/debug.log
+
