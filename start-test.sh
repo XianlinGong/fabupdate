a64f70a146290fe78fc8edec68ab0754284d72ce
Ubuntu <ubuntu@ip-172-31-31-86.ap-northeast-2.compute.internal>
2018-10-24 15:02:05 +0000

    start-test.sh

---
+++ start-test.sh
@@ -0,0 +1,5 @@
+killall fabcoind
+sleep 5
+
+fabcoind -server -listen -rpcuser=fabcoin -rpcpassword=fabcoin -addnode=52.60.97.159 -addnode=54.238.139.252 -addnode=52.78.242.45 -daemon -testnet -txindex=1 -rest -rpcallowip=0.0.0.0/0  -blockmaxconflict=12 -debug
+
